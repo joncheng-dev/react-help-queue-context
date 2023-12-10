4e67cb1d4b13082de7edaa29dedc471f1024316f
Jonathan Cheng <jcheng461@gmail.com>
2023-12-10 13:53:59 -0800

    add context & implement background color / text color themes

---
+++ src/components/ToggleTheme.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ToggleTheme(props) {
+  return (
+    <React.Fragment>
+      <button onClick={props.toggleTheme}>Toggle Theme</button>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+ToggleTheme.propTypes = {
+  toggleTheme: PropTypes.func,
+};
+
+export default ToggleTheme;
